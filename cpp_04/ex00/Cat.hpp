b060c7369f1c2df933786ac35c3472dffdb44631
MajaKaszuba <87532762+MajaKaszuba@users.noreply.github.com>
2025-07-15 16:13:15 +0200

    Add files via upload

---
+++ cpp_04/ex00/Cat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaszuba <mkaszuba@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 12:24:13 by mkaszuba          #+#    #+#             */
+/*   Updated: 2025/07/04 12:29:16 by mkaszuba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+public:
+    Cat();
+    ~Cat();
+
+    void makeSound() const;
+};
+
+#endif
