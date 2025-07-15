44b663e8eb64f18e5fe9a0fc22a2b5e4e2aeb02b
MajaKaszuba <87532762+MajaKaszuba@users.noreply.github.com>
2025-07-15 16:09:14 +0200

    Add files via upload

---
+++ ex00/Dog.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaszuba <mkaszuba@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 12:24:16 by mkaszuba          #+#    #+#             */
+/*   Updated: 2025/07/04 12:25:44 by mkaszuba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog() {
+    type = "Dog";
+    std::cout << "Dog constructor called" << std::endl;
+}
+
+Dog::~Dog() {
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const {
+    std::cout << "Woof woof!" << std::endl;
+}
